83f2873291d34c5a1897b29fe2492a721cdfdef7
liedsong <794243034@qq.com>
2021-09-11 20:55:09 +0800

    3-8 制作CopyUtil封装BeanUtils

--- src/main/java/com/song/wiki/service/EbookService.java
+++ src/main/java/com/song/wiki/service/EbookService.java
@@ -5,7 +5,7 @@ import com.song.wiki.domain.EbookExample;
 import com.song.wiki.mapper.EbookMapper;
 import com.song.wiki.req.EbookReq;
 import com.song.wiki.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.song.wiki.util.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -25,11 +25,15 @@ public class EbookService {
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
         List<EbookResp> respList = new ArrayList<>();
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook,ebookResp);
-            respList.add(ebookResp);
-        }
-        return respList;
+//        for (Ebook ebook : ebookList) {
+//            EbookResp ebookResp = new EbookResp();
+//            BeanUtils.copyProperties(ebook,ebookResp);
+//            //对象复制
+//            EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
+//            respList.add(ebookResp);
+//        }
+        //列表复制
+        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
+        return list;
     }
 }
