0d5260e7e000db4680113ddcd36a691130f9a171
liedsong <794243034@qq.com>
2021-09-11 14:13:57 +0800

    3-5 增加了demo表列表查询

---
+++ src/main/java/com/song/wiki/service/DemoService.java
@@ -0,0 +1,19 @@
+package com.song.wiki.service;
+
+import com.song.wiki.domain.Demo;
+import com.song.wiki.mapper.DemoMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class DemoService {
+
+    @Resource
+    private DemoMapper demoMapper;
+
+    public List<Demo> list(){
+        return demoMapper.selectByExample(null);
+    }
+}
