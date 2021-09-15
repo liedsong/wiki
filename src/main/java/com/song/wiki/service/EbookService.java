4f99df884fc8869a8b83f8ffdf89b0a65d8774ed
liedsong <794243034@qq.com>
2021-09-15 18:26:26 +0800

    6-4 集成PageHelper插件 将mapper日志级别改为trace，打印所有的sql日志

--- src/main/java/com/song/wiki/service/EbookService.java
+++ src/main/java/com/song/wiki/service/EbookService.java
@@ -1,11 +1,15 @@
 package com.song.wiki.service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.song.wiki.domain.Ebook;
 import com.song.wiki.domain.EbookExample;
 import com.song.wiki.mapper.EbookMapper;
 import com.song.wiki.req.EbookReq;
 import com.song.wiki.resp.EbookResp;
 import com.song.wiki.util.CopyUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
@@ -16,6 +20,8 @@ import java.util.List;
 
 @Service
 public class EbookService {
+    
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
 
     @Resource
     private EbookMapper ebookMapper;
@@ -26,7 +32,13 @@ public class EbookService {
         if(!ObjectUtils.isEmpty(req.getName())){
             criteria.andNameLike("%" + req.getName() + "%");
         }
+        //PageHelper只对第一个遇到的sql起作用
+        PageHelper.startPage(1,3);
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        LOG.info("总行数:{}", pageInfo.getTotal());
+        LOG.info("总页数:{}", pageInfo.getPages());
 
         List<EbookResp> respList = new ArrayList<>();
 //        for (Ebook ebook : ebookList) {
