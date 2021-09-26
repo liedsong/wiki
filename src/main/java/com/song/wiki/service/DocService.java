0b0a9e9867d83412a625f4426dcd07d380579ef0
liedsong <794243034@qq.com>
2021-09-26 23:16:08 +0800

    8-11 增加读取文档内容的功能

--- src/main/java/com/song/wiki/service/DocService.java
+++ src/main/java/com/song/wiki/service/DocService.java
@@ -117,4 +117,9 @@ public class DocService {
         criteria.andIdIn(ids);
         docMapper.deleteByExample(docExample);
     }
+
+    public String findContent(Long id){
+        Content content = contentMapper.selectByPrimaryKey(id);
+        return content.getContent();
+    }
 }
--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -190,6 +190,21 @@ export default defineComponent({
       });
     };
 
+    /**
+     * 内容查询
+     */
+    const handleQueryContent = () => {
+
+      axios.get("/doc/find-content/" + doc.value.id).then((response) => {
+        const data = response.data;
+        if(data.success){
+          editor.txt.html(data.content);
+        }else{
+          message.error(data.message);
+        }
+      });
+    };
+
 
     //-------------------表单------------------
     //因为树选择组件的属性状态，会随当前编辑的节点而变化，所以单独用一个响应式变量来表示
@@ -293,6 +308,7 @@ export default defineComponent({
     const edit = (record: any) => {
       modalVisible.value = true;
       doc.value = Tool.copy(record);
+      handleQueryContent();
 
       //不能选择当前节点及其所有子孙节点，作为父节点，会使树断开
       treeSelectData.value = Tool.copy(level1.value);
