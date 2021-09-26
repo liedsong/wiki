e143b659c0514f50669e2a7ca396fa7ef90720b4
liedsong <794243034@qq.com>
2021-09-26 18:22:24 +0800

    8-8 文档内容表的设计与代码生成

--- doc/all.sql
+++ doc/all.sql
@@ -1,3 +1,13 @@
+
+
+-- 文档内容
+drop table if exists `content`;
+create table `content` (
+  `id` bigint not null comment '文档id',
+  `content` mediumtext not null comment '内容',
+  primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='文档内容';
+
 -- 文档表
 drop table if exists `doc`;
 create table `doc` (
