84876635c73658e60d902fcc1df52dd85fd6a6b4
liedsong <794243034@qq.com>
2021-09-26 23:24:49 +0800

    8-11 增加保存文档成功提示 编辑和新增的时候，先清空富文本框

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -226,7 +226,8 @@ export default defineComponent({
         //data=CommonResp
         const data = response.data;
         if(data.success){
-          modalVisible.value = false;
+          // modalVisible.value = false;
+          message.success("保存成功");
 
           //重新加载列表
           handleQuery();
@@ -306,6 +307,8 @@ export default defineComponent({
      * 编辑
      */
     const edit = (record: any) => {
+      //清空富文本框
+      editor.txt.html("");
       modalVisible.value = true;
       doc.value = Tool.copy(record);
       handleQueryContent();
@@ -327,6 +330,8 @@ export default defineComponent({
      * 新增
      */
     const add = () => {
+      //清空富文本框
+      editor.txt.html("");
       modalVisible.value = true;
       doc.value = {
         ebookId: route.query.ebookId
