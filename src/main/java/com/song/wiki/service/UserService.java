8a631602100b83d5a171b003ad0b297c36d72f9d
liedsong <794243034@qq.com>
2021-09-29 23:30:04 +0800

    9-4 增加校验用户名不能重复，增加自定义异常

--- src/main/java/com/song/wiki/service/UserService.java
+++ src/main/java/com/song/wiki/service/UserService.java
@@ -4,6 +4,8 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.song.wiki.domain.User;
 import com.song.wiki.domain.UserExample;
+import com.song.wiki.exception.BusinessException;
+import com.song.wiki.exception.BusinessExceptionCode;
 import com.song.wiki.mapper.UserMapper;
 import com.song.wiki.req.UserQueryReq;
 import com.song.wiki.req.UserSaveReq;
@@ -14,6 +16,7 @@ import com.song.wiki.util.SnowFlake;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
@@ -69,9 +72,17 @@ public class UserService {
     public void save(UserSaveReq req) {
         User user = CopyUtil.copy(req,User.class);
         if(ObjectUtils.isEmpty(req.getId())){
-            //新增
-            user.setId(snowFlake.nextId());
-            userMapper.insert(user);
+            User userDB = selectByLoginName(req.getLoginName());
+            if(ObjectUtils.isEmpty(userDB)){
+                //新增
+                user.setId(snowFlake.nextId());
+                userMapper.insert(user);
+            }else{
+                //用户名已存在
+                throw new BusinessException(BusinessExceptionCode.USER_LOGIN_NAME_EXIST);
+
+            }
+
         } else{
             //更新
             userMapper.updateByPrimaryKey(user);
@@ -81,5 +92,19 @@ public class UserService {
 
     public void delete(Long id){
         userMapper.deleteByPrimaryKey(id);
+    }
+
+    public User selectByLoginName(String LoginName) {
+        UserExample userExample = new UserExample();
+        UserExample.Criteria criteria = userExample.createCriteria();
+        criteria.andLoginNameEqualTo(LoginName);
+
+        //PageHelper只对第一个遇到的sql起作用
+        List<User> userList = userMapper.selectByExample(userExample);
+        if(CollectionUtils.isEmpty(userList)){
+            return null;
+        }else{
+            return userList.get(0);
+        }
     }
 }
