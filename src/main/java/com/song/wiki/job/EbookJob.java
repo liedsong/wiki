32ca81fc98bd334ecbd6cefacdb5f27ca61945c7
liedsong <794243034@qq.com>
2021-10-12 22:19:06 +0800

    10-7 增加日志流水号，方便生产运维

--- src/main/java/com/song/wiki/job/EbookJob.java
+++ src/main/java/com/song/wiki/job/EbookJob.java
@@ -1,8 +1,10 @@
 package com.song.wiki.job;
 
 import com.song.wiki.service.DocService;
+import com.song.wiki.util.SnowFlake;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -16,6 +18,9 @@ public class EbookJob {
     @Resource
     private DocService docService;
 
+    @Resource
+    private SnowFlake snowFlake;
+
     private static final Logger LOG = LoggerFactory.getLogger(EbookJob.class);
 
     /**
@@ -23,6 +28,8 @@ public class EbookJob {
      */
     @Scheduled(cron = "5/30 * * * * ?")
     public void cron() {
+        //内置的函数,增加日志流水号
+        MDC.put("LOG_ID", String.valueOf(snowFlake.nextId()));
         LOG.info("开始更新电子书下的文档数据");
         long start = System.currentTimeMillis();
         docService.updateEbookInfo();
