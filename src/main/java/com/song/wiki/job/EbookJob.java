166e37aa30f2daf4b4db875fc23bba8d574d1e28
liedsong <794243034@qq.com>
2021-10-12 18:31:22 +0800

    10-6 给定时器增加日志

--- src/main/java/com/song/wiki/job/EbookJob.java
+++ src/main/java/com/song/wiki/job/EbookJob.java
@@ -22,8 +22,11 @@ public class EbookJob {
      * 每30秒更新电子书信息
      */
     @Scheduled(cron = "5/30 * * * * ?")
-    public void cron(){
+    public void cron() {
+        LOG.info("开始更新电子书下的文档数据");
+        long start = System.currentTimeMillis();
         docService.updateEbookInfo();
+        LOG.info("更新电子书下的文档数据结束，耗时:{}毫秒",System.currentTimeMillis()-start);
     }
 
 }
